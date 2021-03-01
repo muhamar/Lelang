b73fad151f2acda93d39b3caf431a269fec92ba3
dede ariansya <dedeariansya1@gmail.com>
2021-03-01 11:45:40 +0700

    update api auth

--- application/controllers/Api_auth.php
+++ application/controllers/Api_auth.php
@@ -10,61 +10,88 @@ class Api_auth extends RestController
 {
     const SECRET = 'secret';
 
-
     public function __construct()
     {
         parent::__construct();
+        $this->form_validation->set_error_delimiters('', '');
         $this->load->model('Model_user');
         $this->load->library('form_validation');
     }
 
+    protected function getError(array $arr = [])
+    {
+        if(count($arr)) {
+            foreach($arr as $value) {
+                if($value) return $value;
+            }
+        }
+        return "";
+    }
+
+    protected function createToken(array $user) {
+        $time = time();
+        $exp = $time + 60 * 60 * 24 * 60;
+        return JWT::encode(['id' => $user['id_peserta'], 'iat' => $time, 'exp' => $exp], self::SECRET);
+    }
+
+	protected function isLogin()
+	{
+		$headers = $this->input->request_headers();
+		if(empty($headers['authorization'])) return false;
+
+		try {
+			$token = $headers['authorization'];
+			$decode = JWT::decode($token, self::SECRET, array('HS256'));
+			$id = (int) $decode->id;
+		} catch (Exception $e) {
+			return false;
+		}
+        if (!$id) return false;
+		
+		return $this->Model_user->tampilUser($id);
+	}
+
     public function daftar_post()
     {
-
         $this->form_validation->set_rules('nama', 'Nama', 'required');
         $this->form_validation->set_rules('username', 'Username', 'required|is_unique[peserta.username]');
         $this->form_validation->set_rules('password', 'Password', 'required');
-        if ($this->form_validation->run() == false) {
-            $this->response([
-                'status' => false,
-                'pesan' => 'Form tidak valid'
-                // 'pesan' => "Username / Nohp telah terdaftar !"
-            ], RestController::HTTP_BAD_REQUEST);
-        } else {
-            $data = [
-                'nama' => $this->post('nama'),
-                'username' => $this->post('username'),
-                'password' => $this->post('password')
-            ];
 
-            if ($this->Model_user->tambahUser($data) > 0) {
-                $time = time();
-                $exp = $time + 60 * 60 * 24 * 60;
-
-
-                $peserta = $this->db->get_where('peserta', ['username' => $data['username']])->row_array();
-                $token = JWT::encode(['id' => $peserta['id_peserta'], 'iat' => $time, 'exp' => $exp], self::SECRET);
-                $this->response([
-                    'status' => true,
-                    'pesan' => "Peserta berhasil ditambahkan",
-                    'token' => $token,
-                    'peserta' => $peserta
-                ], RestController::HTTP_CREATED);
-            } else {
-                $this->response([
-                    'status' => false,
-                    'pesan' => 'Peserta gagal ditambahkan'
-                ], RestController::HTTP_BAD_REQUEST);
-            }
+        if (!$this->form_validation->run()) {
+            return $this->response([
+                'status' => 400,
+                'pesan' => $this->getError([
+                    form_error('nama'),
+                    form_error('username'),
+                    form_error('password')
+                ])
+            ], 400);
         }
+
+        $data = [
+            'nama' => $this->post('nama'),
+            'username' => $this->post('username'),
+            'password' => $this->post('password')
+        ];
+
+        if ($this->Model_user->tambahUser($data)) {
+            $peserta = $this->db->get_where('peserta', ['username' => $data['username']])->row_array();
+            return $this->response([
+                'status' => 200,
+                'pesan' => "Peserta berhasil ditambahkan",
+                'token' => $this->createToken($peserta),
+                'peserta' => $peserta
+            ], 200);
+        }
+
+        return $this->response([
+            'status' => 400,
+            'pesan' => 'Peserta gagal ditambahkan'
+        ], 400);
     }
-
-
 
     public function login_post()
     {
-
-
         $username = $this->post('username');
         $password = $this->post('password');
 
@@ -72,100 +99,89 @@ class Api_auth extends RestController
         $this->form_validation->set_rules('password', 'Password', 'required');
 
         if ($this->form_validation->run() == false) {
-            $this->response([
-                'status' => false,
-                'pesan' => 'Username/Password salah!'
-                // 'pesan' => "Username / Nohp telah terdaftar !"
-            ], RestController::HTTP_BAD_REQUEST);
-        } else {
-            $peserta = $this->db->get_where('peserta', ['username' => $username])->row_array();
-            if ($peserta) {
-                if ($password == $peserta['password']) {
-                    $time = time();
-                    $exp = $time + 60 * 60 * 24 * 60;
-                    $token = JWT::encode(['id' => $peserta['id_peserta'], 'iat' => $time, 'exp' => $exp], self::SECRET);
-                    $this->response([
-                        'status' => true,
-                        'peserta' => $peserta,
-                        'token' => $token,
-                        'pesan' => 'berhasil login'
-                    ], RestController::HTTP_OK);
-                } else {
-                    $this->response([
-                        'status' => false,
-                        'pesan' => 'Password Salah !'
-                        // 'pesan' => "Username / Nohp telah terdaftar !"
-                    ], RestController::HTTP_BAD_REQUEST);
-                }
-            } else {
-                $this->response([
-                    'status' => false,
-                    'pesan' => 'Username tidak terdaftar !'
-                    // 'pesan' => "Username / Nohp telah terdaftar !"
-                ], RestController::HTTP_BAD_REQUEST);
-            }
+            return $this->response([
+                'status' => 400,
+                'pesan' => $this->getError([form_error('username'), form_error('password')])
+            ], 400);
         }
+
+		$peserta = $this->db->get_where('peserta', ['username' => $username])->row_array();
+		if ($peserta) {
+			if ($password == $peserta['password']) {
+				return $this->response([
+					'status' => 200,
+					'peserta' => $peserta,
+					'token' => $this->createToken($peserta),
+					'pesan' => 'berhasil login'
+				], 200);
+			}
+			return $this->response([
+				'status' => 400,
+				'pesan' => 'Password Salah !'
+				// 'pesan' => "Username / Nohp telah terdaftar !"
+			], 400);
+		}
+
+		$this->response([
+			'status' => false,
+			'pesan' => 'Username tidak terdaftar !'
+			// 'pesan' => "Username / Nohp telah terdaftar !"
+		], 400);
     }
 
     public function profile_get()
     {
-        $token = $this->input->request_headers()['authorization'];
-        $decode = JWT::decode($token, self::SECRET, array('HS256'));
-        $id = (int) $decode->id;
-
-        if ($id == null) {
-            $peserta = $this->Model_user->tampilUser();
-        } else {
-            $peserta = $this->Model_user->tampilUser($id);
-        }
-
-        if ($peserta) {
-            $this->response([
-                'status' => true,
+		if ($peserta = $this->isLogin()) {
+			$this->response([
+				'status' => true,
                 'data' => $peserta
-            ], RestController::HTTP_OK);
+            ], 200);
         } else {
             $this->response([
-                'status' => false,
-                'pesan' => 'Data tidak ditemukan'
-            ], RestController::HTTP_BAD_REQUEST);
+                'status' => 401,
+                'pesan' => 'mohon login kembali!'
+            ], 401);
         }
     }
 
     public function profile_post()
     {
-        $token = $this->input->request_headers()['authorization'];
-        $decode = JWT::decode($token, self::SECRET, array('HS256'));
-        $id = (int) $decode->id;
+		$peserta = $this->isLogin();
 
+        if (!$peserta) {
+			return $this->response([
+                'status' => 401,
+                'pesan' => 'mohon login kembali!'
+            ], 401);
+        }
 
         $this->form_validation->set_rules('nama', 'Nama', 'required');
         $this->form_validation->set_rules('nohp', 'Nohp', 'required');
         $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-        if (!$this->form_validation->run()) {
-            $this->response([
-                'status' => false,
-                'pesan' => 'Form tidak valid!'
-            ], RestController::HTTP_BAD_REQUEST);
-        } else {
-            $data = [
-                'nama' => $this->post('nama'),
-                'nohp' => $this->post('nohp'),
-                'alamat' => $this->post('alamat')
-            ];
 
-            if ($this->Model_user->updateUser($data, $id) > 0) {
-                $this->response([
-                    'status' => true,
-                    'pesan' => 'Peserta berhasil di update',
-                    'peserta' => $this->db->get_where('peserta', ['id_peserta' => $id])->row_array()
-                ], RestController::HTTP_OK);
-            } else {
-                $this->response([
-                    'status' => false,
-                    'pesan' => "Peserta gagal di update"
-                ], RestController::HTTP_BAD_REQUEST);
-            }
+        if (!$this->form_validation->run()) {
+            return $this->response([
+                'status' => 400,
+                'pesan' => $this->getError([form_error('nama'), form_error('nohp'), form_error('alamat')])
+            ], 400);
         }
+		$data = [
+			'nama' => $this->post('nama'),
+			'nohp' => $this->post('nohp'),
+			'alamat' => $this->post('alamat')
+		];
+
+		if ($this->Model_user->updateUser($data, $peserta['id_peserta']) > 0) {
+			$this->response([
+				'status' => 200,
+				'pesan' => 'Peserta berhasil di update',
+				'peserta' => $this->db->get_where('peserta', ['id_peserta' => $peserta['id_peserta']])->row_array()
+			], 200);
+		} else {
+			$this->response([
+				'status' => false,
+				'pesan' => "Peserta gagal di update"
+			], 400);
+		}
     }
 }
